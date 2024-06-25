65aeae49cf43eb1e5fa0ee54f71f8595b11d1f55
Group034 <Group034@infoway.com>
2024-06-25 17:09:00 +0530

    Day1

--- Assinment1/Assinment1/Program.cs
+++ Assinment1/Assinment1/Program.cs
@@ -10,6 +10,8 @@ namespace Assinment1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome To Mayuresh's Home");
+            Console.ReadKey();
         }
     }
 }
